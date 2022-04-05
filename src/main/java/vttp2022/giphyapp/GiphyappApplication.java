352fe0c7db002d18f6546e40623c8caed1a53a95
kohziyi95 <96542810+kohziyi95@users.noreply.github.com>
2022-04-05 11:07:41 +0800

    initial commit

---
+++ src/main/java/vttp2022/giphyapp/GiphyappApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.giphyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiphyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiphyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/giphyapp/GiphyappApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.giphyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiphyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
